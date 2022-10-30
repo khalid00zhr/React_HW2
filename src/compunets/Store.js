c986aabbec099c6e49bae9d4caadaf89ac9bd7c8
Khalid <khalid00.zhr@gmail.com>
2022-10-30 11:01:59 +0300

    add if for buttons

--- src/compunets/Store.js
+++ src/compunets/Store.js
@@ -9,12 +9,30 @@ import {
     Stack,
     Button,
     useColorModeValue,
+    background,
   } from '@chakra-ui/react';
   
   export default function SocialProfileWithImage(props) {
-    let show=props.show;
-    if(show=true){
-    return (
+    function isAvl(){
+        const min = new Date();  
+        
+        if(min.getMinutes() % 2===0){
+            return ( <Button  w={'full'}  mt={8} color={'white'}rounded={'md'}bg={'green'}
+        
+                       >
+              Register
+            </Button>)
+        }
+        else{
+            return (<Button  w={'full'}  mt={8} color={'white'}rounded={'md'}bg={'red'}
+        
+                    >
+           closed
+         </Button>)
+
+        }
+     }
+       return (
         
       <Center py={6}>
         <Box
@@ -39,12 +57,8 @@ import {
               <Text color={'gray.500'}>{props.des}</Text>
             </Stack>
             
-            
-    <Button  w={'full'}  mt={8} color={'white'}rounded={'md'}
-           return bg={useColorModeValue('green.500', 'gray.900')}
-                       >
-              Register
-            </Button>
+            {isAvl()}
+    
              
            
             
@@ -53,51 +67,4 @@ import {
           </Box>
         </Box>
       </Center>
-    );
-  }}
-
-  else  {
-    return (
-        
-        <Center py={6}>
-          <Box
-            maxW={'270px'}
-            w={'full'}
-            bg={useColorModeValue('white', 'gray.800')}
-            boxShadow={'2xl'}
-            rounded={'md'}
-            overflow={'hidden'}>
-            <Image
-              h={'120px'}
-              w={'full'}
-              src={props.pic }
-              objectFit={'cover'}
-            />
-              
-            <Box p={6}>
-              <Stack spacing={0} align={'center'} mb={5}>
-                <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
-                {props.name}
-                </Heading>
-                <Text color={'gray.500'}>{props.des}</Text>
-              </Stack>
-              
-              
-      <Button  w={'full'}  mt={8} color={'white'}rounded={'md'}
-             return bg={useColorModeValue('green.500', 'gray.900')}
-                         >
-                Register
-              </Button>
-               
-             
-              
-              
-      
-            </Box>
-          </Box>
-        </Center>
-      );
-    }
-
-
-  
+    );}
